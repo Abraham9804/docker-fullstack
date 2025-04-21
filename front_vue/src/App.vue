fd8e058fc0827596dcb97dde5a1c9242a5a86241
Abraham Romero <romero9804@hotmail.com>
2025-04-21 14:55:48 -0700

    proyecto terminado

--- front_vue/src/App.vue
+++ front_vue/src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from "axios"
+    import {ref} from "vue"
+
+    const usuarios = ref([])
+    async function cargarDatos(){
+        const {data} = await axios.get("http://localhost:3000/api/user")
+        usuarios.value = data
+    }
+
+    cargarDatos()
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Listar Usuarios</h1>
+  <table border="1">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Accion</th>
+        </tr>
+    </thead>
+    <tbody>
+      {{ usuarios }}
+    </tbody>
+  </table>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
